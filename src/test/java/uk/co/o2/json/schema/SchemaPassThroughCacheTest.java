deca86ef99608fa38da467979736bc5c50ad3588
Sean Reilly <seanjreilly@gmail.com>
2012-04-17 15:28:47 +0100

    Optimized imports.

--- src/test/java/uk/co/o2/json/schema/SchemaPassThroughCacheTest.java
+++ src/test/java/uk/co/o2/json/schema/SchemaPassThroughCacheTest.java
@@ -1,20 +1,18 @@
 package uk.co.o2.json.schema;
 
-import java.net.URL;
-import java.util.concurrent.ConcurrentMap;
 import org.codehaus.jackson.JsonFactory;
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import uk.co.o2.json.schema.SchemaPassThroughCache.SchemaCompilerFactory;
-import static org.junit.Assert.assertTrue;
+
+import java.net.URL;
+import java.util.concurrent.ConcurrentMap;
+
 import static org.junit.Assert.assertSame;
-import static org.mockito.Matchers.isNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.mock;
-import org.mockito.ArgumentMatcher;
-import org.hamcrest.Matcher;
-import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.when;
 
 public class SchemaPassThroughCacheTest {
